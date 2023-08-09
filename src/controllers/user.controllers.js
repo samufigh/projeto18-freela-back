a5a37a0e636ee95963693bc314b02a25df86a5f5
samufigh <samufigh21@gmail.com>
2023-08-09 11:38:24 -0300

    feat: repository pattern

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,18 +1,16 @@
 import bcrypt from "bcrypt"
-import { db } from "../database/database.connection.js"
 import { v4 as uuid } from "uuid"
+import { checkEmail, createSession, deleteSession, getUserByEmail, registerUser } from "../repository/user.repository.js"
 
 export async function register(req, res) {
     const { name, cpf, telephone, email, password, confirmPassword } = req.body
     const hash = bcrypt.hashSync(password, 10)
     try {
         if (confirmPassword !== password) return res.status(422).send("As senhas não coincidem")
-        const existingEmail = await db.query(`SELECT * FROM users WHERE email=$1;`, [email])
-        if (existingEmail.rowCount > 0) return res.status(409).send("Esse email já está cadastrado")
+        const existingEmail = await checkEmail(email, cpf, telephone)
+        if (existingEmail.rowCount > 0) return res.status(409).send("Email, CPF ou Telefone já cadastrado")
 
-        await db.query(`
-            INSERT INTO users (name, cpf, telephone, email, password) 
-            VALUES ($1, $2, $3, $4, $5);`, [name, cpf, telephone, email, hash])
+        await registerUser(name, cpf, telephone, email, hash)
 
         res.sendStatus(201)
     } catch (err) {
@@ -23,15 +21,14 @@ export async function register(req, res) {
 export async function login(req, res) {
     const { email, password } = req.body
     try {
-        const user = await db.query(`SELECT id, email, password FROM users WHERE email=$1;`, [email])
+        const user = await getUserByEmail(email)
 
         if (user.rows[0] && bcrypt.compareSync(password, user.rows[0].password)) {
             const token = uuid()
-            await db.query(`DELETE FROM sessions WHERE "userId"=$1;`, [user.rows[0].id])
 
-            await db.query(`
-                INSERT INTO sessions ("userId", "token") 
-                VALUES ($1, $2);`, [user.rows[0].id, token])
+            await deleteSession(user)
+
+            await createSession(user, token)
 
             res.send({token})
 
---
+++ src/repository/user.repository.js
@@ -0,0 +1,27 @@
+import { db } from "../database/database.connection.js"
+
+export function checkEmail(email, cpf, telephone){
+    const result = db.query(`SELECT * FROM users WHERE email=$1 OR cpf=$2 OR telephone=$3;`, [email, cpf, telephone])
+    return result
+}
+
+export function registerUser(name, cpf, telephone, email, hash){
+        db.query(`
+            INSERT INTO users (name, cpf, telephone, email, password) 
+            VALUES ($1, $2, $3, $4, $5);`, [name, cpf, telephone, email, hash])
+}
+
+export function getUserByEmail(email){
+    const result = db.query(`SELECT id, email, password FROM users WHERE email=$1;`, [email])
+    return result
+}
+
+export function deleteSession(user){
+    db.query(`DELETE FROM sessions WHERE "userId"=$1;`, [user.rows[0].id])
+}
+
+export function createSession(user, token){
+    db.query(`
+                INSERT INTO sessions ("userId", "token") 
+                VALUES ($1, $2);`, [user.rows[0].id, token])
+}
