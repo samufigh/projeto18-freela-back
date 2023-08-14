1773ad916fdb9d15d535fe3465441eec47c26492
samufigh <samufigh21@gmail.com>
2023-08-14 13:23:21 -0300

    fix: rota POST/ model

--- src/schemas/model.schemas.js
+++ src/schemas/model.schemas.js
@@ -1,7 +1,7 @@
 import joi from "joi"
 
 export const schemaModel = joi.object({
-    name: joi.string().required(),
+    name: joi.string().max(10).required(),
     picture: joi.string().regex(/^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/).required(),
     pictureUserPet: joi.string().regex(/^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/).required(),
     description: joi.string().required()
